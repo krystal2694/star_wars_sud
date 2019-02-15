8672c20644f12f8cf86e3b7c4eb8c291c7742c8a
krystal2694 <pwong2632my.bcit.ca>
2019-02-14 20:26:17 -0800

    added more unit tests for generate syllable

--- A2/test_generate_syllable.py
+++ A2/test_generate_syllable.py
@@ -13,3 +13,12 @@ class TestGenerateSyllable(TestCase):
     def test_generate_syllable_is_letter(self):
         self.assertIn(dungeonsanddragons.generate_syllable()[0], string.ascii_letters)
         self.assertIn(dungeonsanddragons.generate_syllable()[1], string.ascii_letters)
+
+    def test_generate_syllable_first_index_is_consonant(self):
+        consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
+                      'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+        self.assertIn(dungeonsanddragons.generate_syllable()[0], consonants)
+
+    def test_generate_syllable_second_index_is_vowel(self):
+        vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+        self.assertIn(dungeonsanddragons.generate_syllable()[1], vowels)
