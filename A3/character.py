8f9804b1830e712def5e85742a97b1481950ed75
krystal2694 <pwong2632my.bcit.ca>
2019-03-01 01:21:29 -0800

    Added create pokemon function!

--- A3/character.py
+++ A3/character.py
@@ -1,16 +1,18 @@
 """Create a Pokemon Character."""
+from random import randint
 # A01089672
 # Krystal Wong
 
 
-def choose_character_name():
+def choose_pokemon_name():
     name = input("What is your name? ")
     return name.title()
 
 
-def choose_class():
+def choose_pokemon_type():
     """Return user's selection of their desired Pokémon type."""
 
+    print("What type of Pokemon are you?")
     # provide user with information needed to make their choice
     classes = {1: "water", 2: "fire", 3: "electric", 4: "fairy", 5: "psychic", 6: "ice",
                7: "grass", 8: "poison", 9: "ghost", 10: "steel", 11: "bug", 12: "dark"}
@@ -26,4 +28,24 @@ def choose_class():
             return char_class
     else:
         print("You must choose a Pokémon type from the list above.")
-        return choose_class()
+        return choose_pokemon_type()
+
+
+def create_pokemon(name):
+    """Create a Pokemon.
+
+    PARAM: name, a string
+    PRECONDITION: name must be a string
+    POSTCONDITION: create a Pokemon complete with name, Type, HP, Dexterity
+    RETURN: a dictionary consisting of all information required for a new Pokemon
+    """
+
+    pokemon_type = choose_pokemon_type()
+
+    pokemon = {"Name": name,
+               "Type": pokemon_type,
+               "HP": 10,
+               "Dexterity": randint(1, 5)}
+
+    return pokemon
+
