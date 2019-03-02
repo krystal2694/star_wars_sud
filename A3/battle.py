4b3673a66723f572fdb7a4ac2189ac277d787a6b
krystal2694 <pwong2632my.bcit.ca>
2019-03-02 12:29:00 -0800

    Added while loop in encounter pokemon to re-ask user if user does not enter "f" or "r"

--- A3/battle.py
+++ A3/battle.py
@@ -38,7 +38,6 @@ def combat_round(pokemon, wild_pokemon):
     PRECONDITION: opponent_one must be a dictionary containing a complete pokemon
     PRECONDITION: opponent_two must be a dictionary containing a complete pokemon
     """
-
     while pokemon["HP"] > 0 and wild_pokemon["HP"] > 0:
         pokemon = attack(wild_pokemon, pokemon)
         if pokemon["HP"] <= 0:
@@ -46,12 +45,15 @@ def combat_round(pokemon, wild_pokemon):
         else:
             wild_pokemon = attack(pokemon, wild_pokemon)
 
+    if pokemon["HP"] > 0:
+        print("%s's HP is %d." % (pokemon["Name"], pokemon["HP"]))
+    wild_pokemon["HP"] = 5
     return pokemon
 
 
-pokedex = [{"Name": "Snorlax", "Type": "Normal", "HP": 5, "Dexterity": 7},
-           {"Name": "Jiggly Puff", "Type": "Fairy", "HP": 5, "Dexterity": 7},
-           {"Name": "Rattata", "Type": "Normal", "HP": 5, "Dexterity": 6},
+pokedex = [{"Name": "Snorlax", "Type": "Normal", "HP": 5, "Dexterity": 5},
+           {"Name": "Jiggly Puff", "Type": "Fairy", "HP": 5, "Dexterity": 6},
+           {"Name": "Rattata", "Type": "Normal", "HP": 5, "Dexterity": 4},
            {"Name": "Ghastly", "Type": "Ghost", "HP": 5, "Dexterity": 6},
            {"Name": "Charmander", "Type": "Fire", "HP": 5, "Dexterity": 9}]
 
@@ -59,21 +61,22 @@ pokedex = [{"Name": "Snorlax", "Type": "Normal", "HP": 5, "Dexterity": 7},
 def encounter_pokemon(pokemon):
     if randint(1, 10) == 1:
         wild_pokemon = pokedex[randint(0, 4)]
-        fight_or_run = input("A wild " + wild_pokemon["Name"] + " appeared! 'f' to fight, 'r' to run: ").strip().lower()
-        if fight_or_run == "f":
-            combat_round(pokemon, wild_pokemon)
-            if pokemon["HP"] <= 0:
-                return pokemon
-            else:
-                print("%s's HP is %d." % (pokemon["Name"], pokemon["HP"]))
-        elif fight_or_run == "r":
-            if randint(1, 10) == 1:
-                damage = randint(1, 4)
-                pokemon["HP"] -= damage
-                print("%s struck you as you fled! %s has taken a %d point hit."
-                      % (wild_pokemon["Name"], pokemon["Name"], pokemon["HP"]))
-            else:
-                print("You have fled successfully!")
+        fight_or_run = 0
+        while fight_or_run != "f" and fight_or_run != "r":
+            fight_or_run = input("A wild %s appeared! Your current have %dHP. 'f' to fight, 'r' to run: "
+                                 % (wild_pokemon["Name"], pokemon["HP"])).strip().lower()
+            if fight_or_run == "f":
+                combat_round(pokemon, wild_pokemon)
+                if pokemon["HP"] <= 0:
+                    return pokemon
+            elif fight_or_run == "r":
+                if randint(1, 10) == 1:
+                    damage = randint(1, 4)
+                    pokemon["HP"] -= damage
+                    print("%s struck you as you fled! %s has taken a %d point hit."
+                          % (wild_pokemon["Name"], pokemon["Name"], pokemon["HP"]))
+                else:
+                    print("You have fled successfully!")
         sud.print_game_map(sud.create_game_map(pokemon))
     return pokemon
 
