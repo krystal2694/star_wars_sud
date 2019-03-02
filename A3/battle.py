f7b63c991baa5ac235d53f1d47a8990ae8d7ae9e
krystal2694 <pwong2632my.bcit.ca>
2019-03-01 16:00:29 -0800

    Added attack function in battle.py

--- A3/battle.py
+++ A3/battle.py
@@ -22,3 +22,32 @@ def determine_order():
         return False
     elif opponent_one_roll == opponent_two_roll:
         return determine_order()
+
+
+def attack(offence_char, defence_char):
+    """Simulate the attack of the character on offence on the character on defense.
+
+    PARAM: offence_char, a dictionary
+    PARAM: defence_chr, a dictionary
+    PRECONDITION: offence_char must be a dictionary containing a complete character
+    PRECONDITION: defence_char must be a dictionary containing a complete character
+    POSTCONDITION: determine if the attack was successful, modify defence_char's HP accordingly
+    RETURN: defence_char as a dictionary """
+
+    print(offence_char["Name"] + " strikes!")
+    damage = randint(1, 6)
+
+    offence_attack = randint(1, 10)
+    if offence_attack > defence_char["Dexterity"]:
+        defence_char["HP"] -= damage
+        print(defence_char["Name"] + " has taken a " + str(damage) + " point hit!")
+        if defence_char["HP"] <= 0:
+            print(defence_char["Name"] + " has perished.")
+            print(offence_char["Name"] + " is the winner!")
+        else:
+            print(defence_char["Name"] + "'s HP has dropped to " + str(defence_char["HP"]) + ".")
+    else:
+        print(offence_char["Name"] + " missed!")
+    return defence_char
+
+
