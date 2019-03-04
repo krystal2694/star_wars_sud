ab7dc74a04ac38b63932542beca2382e1c04bf23
krystal2694 <pwong2632my.bcit.ca>
2019-03-04 14:31:16 -0800

    modified choose rebel class function print statements

--- A3/character.py
+++ A3/character.py
@@ -15,20 +15,20 @@ def choose_name():
 rebel_class_dict = {1: ["Knowledge", "Jedi"], 2: ["Strength", "Rebel Fighter"], 3: ["Wit", "Smuggler"]}
 
 
-def choose_rebel_class():
+def choose_rebel_class(name):
     """Return user's selection of their desired rebel class."""
 
-    for key, value in rebel_class_dict.items():
-        print(key, value[0])
+    selection = input("\n" + line + "\nTell me, %s, what do you consider to be your most valuable trait?\n\n"
+                                    "1 Knowledge\n2 Strength\n3 Wit\n\nEnter the corresponding number: " % name).strip()
 
-    selection = int(input("\n" + "Enter the corresponding number: ").strip())
-    print("\n" + line)
     for number, rebel_class in rebel_class_dict.items():
-        if selection == number:
+        if int(selection) == number:
+            print("\n" + line + "\nAh! You would make a great.. %s!\n" % rebel_class[1] +
+                  "\nNow, come with me %s.\n\nWe have a galaxy to save!\n\n" % name + line)
             return rebel_class[1]
     else:
         print("You must choose one from the list above.")
-        return choose_rebel_class()
+        return choose_rebel_class(name)
 
 
 def create_rebel(name, rebel_class):
@@ -65,8 +65,8 @@ def print_rebel(rebel):
 
 
 def main():
-    print(choose_rebel_class())
-
+    # print(choose_rebel_class())
+    pass
 
 if __name__ == '__main__':
     main()
