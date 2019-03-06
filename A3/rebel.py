2c22580472f866f919a675acdca7f0a82cbad052
krystal2694 <pwong2632my.bcit.ca>
2019-03-05 16:47:23 -0800

    added set coordinates function to move character

--- A3/rebel.py
+++ A3/rebel.py
@@ -8,7 +8,7 @@ def get_name():
     return rebel["Name"]
 
 
-def set_name():
+def choose_name():
     name = input("What is your name, young one? ")
     rebel["Name"] = name
 
@@ -17,13 +17,35 @@ def get_hp():
     return rebel["HP"]
 
 
-def increment_hp(integer):
+def increment_hp():
     rebel["HP"] += 1
     return rebel
 
 
+def decrease_hp(damage):
+    rebel["HP"] -= damage
+
+
 def get_dexterity():
     return rebel["Dexterity"]
+
+
+def get_coordinates():
+    return rebel["Coordinates"]
+
+
+def set_coordinates(direction):
+    if direction == "n" and rebel["Coordinates"][0] != 0:
+            rebel["Coordinates"][0] -= 1
+    elif direction == "s" and rebel["Coordinates"][0] != 10:
+            rebel["Coordinates"][0] += 1
+    elif direction == "e" and rebel["Coordinates"][1] != 10:
+            rebel["Coordinates"][1] += 1
+    elif direction == "w" and rebel["Coordinates"][1] != 0:
+            rebel["Coordinates"][1] -= 1
+    else:
+        print("Do not leave the galaxy %s, you cannot leave us in the hands of the Galactic Empire!" % rebel["Name"])
+        return rebel
 
 
 
