d17f103982d4f61a47783cd6c8256b989c058984
krystal2694 <pwong2632my.bcit.ca>
2019-03-05 16:54:32 -0800

    copied choose rebel class function from character file to rebel file

--- A3/rebel.py
+++ A3/rebel.py
@@ -1,6 +1,10 @@
 """Rebel character for Star Wars game."""
 from random import randint
 
+line = "-------------------------------------------------------------------\n"
+
+rebel_class_dict = {"1": ["Knowledge", "Jedi"], "2": ["Strength", "Rebel Fighter"], "3": ["Wit", "Smuggler"]}
+
 rebel = {"Name": "", "Class": "", "HP": 10, "Dexterity": randint(5, 10), "Coordinates": [5, 5]}
 
 
@@ -9,8 +13,22 @@ def get_name():
 
 
 def choose_name():
-    name = input("What is your name, young one? ")
-    rebel["Name"] = name
+    rebel["Name"] = input("What is your name, young one? ").title()
+
+
+def choose_rebel_class(name):
+    """Return user's selection of their desired rebel class."""
+
+    selection = input("\n" + line + "\nTell me, %s, what do you consider to be your most valuable trait?\n\n"
+                                    "1 Knowledge\n2 Strength\n3 Wit\n\nEnter the corresponding number: " % name).strip()
+    for number, rebel_class in rebel_class_dict.items():
+        if selection == number:
+            print("\n" + line + "\nAh! I think you would make a great.. %s!\n" % rebel_class[1] +
+                  "\nNow, come with me %s.\n\nWe have a galaxy to save!\n\n" % name + line)
+            return rebel_class[1]
+    else:
+        print("You must choose one from the list above.")
+        return choose_rebel_class(name)
 
 
 def get_hp():
