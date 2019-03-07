250f148edc56467e0216931b8d56095765ef6157
krystal2694 <pwong2632my.bcit.ca>
2019-03-06 19:29:26 -0800

    modified imperial forces dictionaries, refactored parameters for imperial functions

--- A3/imperial.py
+++ A3/imperial.py
@@ -1,33 +1,33 @@
 """Imperial forces that the player may encounter."""
 
-imperial_forces = [{"Name": "Stormtrooper", "Type": "imperial", "HP": 5, "Dexterity": 2},
-                   {"Name": "Shocktrooper", "Type": "imperial", "HP": 5, "Dexterity": 3},
-                   {"Name": "Imperial Officer", "Type": "imperial", "HP": 5, "Dexterity": 4},
-                   {"Name": "Bounty Hunter", "Type": "imperial", "HP": 5, "Dexterity": 5},
-                   {"Name": "Imperial Spy", "Type": "imperial", "HP": 5, "Dexterity": 5},
-                   {"Name": "Sith Lord", "Type": "imperial", "HP": 5, "Dexterity": 6},
-                   {"Name": "AT-AT Walker", "Type": "imperial", "HP": 5, "Dexterity": 7}]
+imperial_forces = [{"Name": "Stormtrooper", "HP": 5, "Dexterity": 2},
+                   {"Name": "Shocktrooper", "HP": 5, "Dexterity": 3},
+                   {"Name": "Imperial Officer", "HP": 5, "Dexterity": 4},
+                   {"Name": "Bounty Hunter",  "HP": 5, "Dexterity": 5},
+                   {"Name": "Imperial Spy", "HP": 5, "Dexterity": 5},
+                   {"Name": "Sith Lord", "HP": 5, "Dexterity": 6},
+                   {"Name": "AT-AT Walker", "HP": 5, "Dexterity": 7}]
 
 
-def get_name(imperial_index):
-    return imperial_forces[imperial_index]["Name"]
+def get_name(index):
+    return imperial_forces[index]["Name"]
 
 
-def get_hp(imperial_index):
-    return imperial_forces[imperial_index]["HP"]
+def get_hp(index):
+    return imperial_forces[index]["HP"]
 
 
-def set_hp(imperial_index, new_hp):
+def set_hp(index, new_hp):
     global imperial_forces
-    imperial_forces[imperial_index]["HP"] = new_hp
+    imperial_forces[index]["HP"] = new_hp
 
 
-def decrease_hp(imperial_index, damage):
+def decrease_hp(index, damage):
     global imperial_forces
-    imperial_forces[imperial_index]["HP"] -= damage
+    imperial_forces[index]["HP"] -= damage
 
 
-def get_dexterity(imperial_index):
-    return imperial_forces[imperial_index]["Dexterity"]
+def get_dexterity(index):
+    return imperial_forces[index]["Dexterity"]
 
 
