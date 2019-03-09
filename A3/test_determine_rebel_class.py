6636a3efe5e2679a0a51e04f0ae0b16bc53d3700
krystal2694 <pwong2632my.bcit.ca>
2019-03-09 10:54:13 -0800

    Completed unit tests for determine rebel class

---
+++ A3/test_determine_rebel_class.py
@@ -0,0 +1,62 @@
+from unittest import TestCase
+from unittest.mock import patch
+from sud import determine_rebel_class
+import rebel
+import io
+
+
+class TestDetermineRebelClass(TestCase):
+    def setUp(self):
+        rebel.rebel["Name"] = "Chris"
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', return_value="1")
+    def test_determine_rebel_class_with_choice_knowledge_jedi(self, mock_input, mock_stdout):
+        expected_output = "\n-------------------------------------------------------------------\n\n"\
+                          "Ah! I think you would make a great.. Jedi!\n\n" \
+                          "Now, come with me Chris.\n\n"\
+                          "We have a galaxy to save!\n\n"\
+                          "-------------------------------------------------------------------\n\n"
+        determine_rebel_class()
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', return_value="2")
+    def test_determine_rebel_class_with_choice_strength_rebel_fighter(self, mock_input, mock_stdout):
+        expected_output = "\n-------------------------------------------------------------------\n\n"\
+                          "Ah! I think you would make a great.. Rebel Fighter!\n\n" \
+                          "Now, come with me Chris.\n\n"\
+                          "We have a galaxy to save!\n\n"\
+                          "-------------------------------------------------------------------\n\n"
+        determine_rebel_class()
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', return_value="3")
+    def test_determine_rebel_class_with_choice_wit_smuggler(self, mock_input, mock_stdout):
+        expected_output = "\n-------------------------------------------------------------------\n\n"\
+                          "Ah! I think you would make a great.. Smuggler!\n\n" \
+                          "Now, come with me Chris.\n\n"\
+                          "We have a galaxy to save!\n\n"\
+                          "-------------------------------------------------------------------\n\n"
+        determine_rebel_class()
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=["12", "two", "g", "1"])
+    def test_determine_rebel_class_ask_user_input_until_input_is_valid(self, mock_input, mock_stdout):
+        expected_output = "You must choose one from the list above.\n" \
+                          "You must choose one from the list above.\n" \
+                          "You must choose one from the list above.\n" \
+                          "\n-------------------------------------------------------------------\n\n"\
+                          "Ah! I think you would make a great.. Jedi!\n\n" \
+                          "Now, come with me Chris.\n\n"\
+                          "We have a galaxy to save!\n\n"\
+                          "-------------------------------------------------------------------\n\n"
+        determine_rebel_class()
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', return_value="1")
+    def test_determine_rebel_class_with_valid_input_return_none(self, mock_input, mock_stdout):
+        self.assertIsNone(determine_rebel_class())
