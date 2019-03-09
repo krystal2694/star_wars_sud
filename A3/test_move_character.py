253edd004ca097b20662ad7fd078dfa6f2311bf4
krystal2694 <pwong2632my.bcit.ca>
2019-03-09 11:41:05 -0800

    Tweaked unit tests for move character

--- A3/test_move_character.py
+++ A3/test_move_character.py
@@ -1,28 +1,27 @@
 from unittest import TestCase
 from sud import move_character
-from rebel import get_row
-from rebel import get_column
+from rebel import rebel
 # I do not test this function with invalid arguments because an if statement
 # in the game loop function already accounts for invalid arguments
 
 
 class TestMoveCharacter(TestCase):
     def test_move_character_north(self):
-        row_before_move = get_row()
+        row_before_move = rebel["Row"]
         move_character("n")
-        self.assertEqual(get_row(), row_before_move - 1)
+        self.assertEqual(rebel["Row"], row_before_move - 1)
 
     def test_move_character_south(self):
-        row_before_move = get_row()
+        row_before_move = rebel["Row"]
         move_character("s")
-        self.assertEqual(get_row(), row_before_move + 1)
+        self.assertEqual(rebel["Row"], row_before_move + 1)
 
     def test_move_character_east(self):
-        column_before_move = get_column()
+        column_before_move = rebel["Column"]
         move_character("e")
-        self.assertEqual(get_column(), column_before_move + 1)
+        self.assertEqual(rebel["Column"], column_before_move + 1)
 
     def test_move_character_west(self):
-        column_before_move = get_column()
+        column_before_move = rebel["Column"]
         move_character("w")
-        self.assertEqual(get_column(), column_before_move - 1)
+        self.assertEqual(rebel["Column"], column_before_move - 1)
