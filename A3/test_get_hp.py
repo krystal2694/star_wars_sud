30d8409cc9fd0401ca7cedbffe36cc4fedf8da70
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 23:03:33 -0800

    added return type unit test for get hp

--- A3/test_get_hp.py
+++ A3/test_get_hp.py
@@ -1,7 +1,15 @@
 from unittest import TestCase
 from rebel import get_hp
+import rebel
 
 
 class TestGetHp(TestCase):
     def test_get_hp_before_game_play(self):
-        self.assertEqual(get_hp(), 10)
+        self.assertIs(get_hp(), rebel.rebel["HP"])
+
+    def test_get_hp_after_modified_hp(self):
+        rebel.set_hp(5)
+        self.assertIs(get_hp(), rebel.rebel["HP"])
+
+    def test_get_hp_return_type(self):
+        self.assertIsInstance(get_hp(), int)
