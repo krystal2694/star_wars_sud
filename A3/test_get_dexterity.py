ae7c703a7602901d843e43c1a7d577e872bfc3a0
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 23:38:14 -0800

    added unit test for return type

--- A3/test_get_dexterity.py
+++ A3/test_get_dexterity.py
@@ -1,7 +1,11 @@
 from unittest import TestCase
 from rebel import get_dexterity
+import rebel
 
 
 class TestGetDexterity(TestCase):
     def test_get_dexterity(self):
-        self.assertEqual(get_dexterity(), 5)
+        self.assertEqual(get_dexterity(), rebel.rebel["Dexterity"])
+
+    def test_get_dexterity_return_type(self):
+        self.assertIsInstance(get_dexterity(), int)
