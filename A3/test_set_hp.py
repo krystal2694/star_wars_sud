2be81d04f33f9b5e0b5c3d7fa84efccd4b619a82
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 23:23:22 -0800

    added unit test to prove value is changed from original

--- A3/test_set_hp.py
+++ A3/test_set_hp.py
@@ -1,17 +1,22 @@
 from unittest import TestCase
 from rebel import set_hp
-from rebel import rebel
+import rebel
 
 
 class TestSetHp(TestCase):
     def test_set_hp_with_0(self):
         set_hp(0)
-        self.assertEqual(rebel["HP"], 0)
+        self.assertEqual(rebel.rebel["HP"], 0)
 
     def test_set_hp_with_positive_int(self):
         set_hp(8)
-        self.assertEqual(rebel["HP"], 8)
+        self.assertEqual(rebel.rebel["HP"], 8)
 
     def test_set_hp_with_negative_int(self):
         set_hp(-12)
-        self.assertEqual(rebel["HP"], -12)
+        self.assertEqual(rebel.rebel["HP"], -12)
+
+    def test_set_hp_changed_from_original(self):
+        original = rebel.get_hp()
+        set_hp(4)
+        self.assertIsNot(original, rebel.rebel["HP"])
