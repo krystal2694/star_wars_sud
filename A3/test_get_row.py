349e5ac9161c55c0b5c7e4f861a5ced4a52f06b4
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 23:05:28 -0800

    added return type unit test for get row

--- A3/test_get_row.py
+++ A3/test_get_row.py
@@ -4,4 +4,7 @@ from rebel import get_row
 
 class TestGetRow(TestCase):
     def test_get_row_before_game_play(self):
-        self.assertEqual(get_row(), 5)
+        self.assertIs(get_row(), 5)
+
+    def test_get_row_return_type(self):
+        self.assertIsInstance(get_row(), int)
