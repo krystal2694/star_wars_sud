e0b7a33bd5b5c6731d8a3e2791f718c0320f0c36
krystal2694 <pwong2632my.bcit.ca>
2019-03-09 12:24:01 -0800

    completed unit tests for heal character

--- A3/test_heal_character.py
+++ A3/test_heal_character.py
@@ -13,17 +13,24 @@ class TestHealCharacter(TestCase):
             heal_character()
             self.assertEqual(rebel["HP"], hp_before_heal + 1)
 
-    def test_heal_character_when_hp_less_than_10_print_output(self):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_heal_character_when_hp_less_than_10_print_output(self, mock_stdout):
         for i in range(1, 10):
             rebel["HP"] = i
-            hp_before_heal = rebel["HP"]
             heal_character()
-            self.assertEqual(rebel["HP"], hp_before_heal + 1)
+        expected_output = "You're healing! Your HP is 2.\n" \
+                          "You're healing! Your HP is 3.\n" \
+                          "You're healing! Your HP is 4.\n" \
+                          "You're healing! Your HP is 5.\n" \
+                          "You're healing! Your HP is 6.\n" \
+                          "You're healing! Your HP is 7.\n" \
+                          "You're healing! Your HP is 8.\n" \
+                          "You're healing! Your HP is 9.\n" \
+                          "You're healing! Your HP is 10.\n"
+        self.assertEqual(expected_output, mock_stdout.getvalue())
 
     def test_heal_character_when_at_full_health(self):
         rebel["HP"] = 10
         hp_before_heal = rebel["HP"]
         heal_character()
         self.assertEqual(rebel["HP"], hp_before_heal)
-
-
