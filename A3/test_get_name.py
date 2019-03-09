bbe975e559d13241092adc449c5b247f6f045eb7
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 22:59:39 -0800

    added return type unit test for get name

--- A3/test_get_name.py
+++ A3/test_get_name.py
@@ -1,14 +1,17 @@
 from unittest import TestCase
 from unittest.mock import patch
+from rebel import get_name
 import rebel
-# must run unit tests separately
 
 
 class TestGetName(TestCase):
     def test_get_name_before_play_enters_name(self):
-        self.assertEqual(rebel.rebel["Name"], "")
+        self.assertIs(get_name(), rebel.rebel["Name"])
 
     @patch('builtins.input', return_value="Han Solo")
     def test_get_name_after_player_enters_name(self, mock_input):
         rebel.set_name()
-        self.assertEqual(rebel.rebel["Name"], "Han Solo")
+        self.assertEqual(get_name(), "Han Solo")
+
+    def test_get_name_return_type(self):
+        self.assertIsInstance(get_name(), str)
