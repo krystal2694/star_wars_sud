076edfa002ebaf65b4463a84053e7cbc54b5058b
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 23:31:30 -0800

    added unit test to prove value is changed from original

--- A3/test_set_row.py
+++ A3/test_set_row.py
@@ -1,9 +1,14 @@
 from unittest import TestCase
 from rebel import set_row
-from rebel import rebel
+import rebel
 
 
 class TestSetRow(TestCase):
-    def test_set_row(self):
+    def test_set_row_value_reflects_parameter_passed(self):
         set_row(4)
-        self.assertEqual(rebel["Row"], 4)
+        self.assertEqual(rebel.rebel["Row"], 4)
+
+    def test_set_row_changed_from_original(self):
+        original = rebel.get_row()
+        set_row(6)
+        self.assertIsNot(original, rebel.get_row())
