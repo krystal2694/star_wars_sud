f5c4786275ad890db02305b87287649752a09cf0
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 21:20:07 -0800

    completed unit tests for set hp

--- A3/test_set_hp.py
+++ A3/test_set_hp.py
@@ -3,10 +3,14 @@ import rebel
 
 
 class TestSetHp(TestCase):
-
-    def setUp(self):
-        rebel.set_hp(10)
-
-    def test_set_hp_0(self):
+    def test_set_hp_with_0(self):
         rebel.set_hp(0)
         self.assertEqual(rebel.rebel["HP"], 0)
+
+    def test_set_hp_with_positive_int(self):
+        rebel.set_hp(8)
+        self.assertEqual(rebel.rebel["HP"], 8)
+
+    def test_set_hp_with_negative_int(self):
+        rebel.set_hp(-12)
+        self.assertEqual(rebel.rebel["HP"], -12)
