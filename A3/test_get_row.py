7a07e8229552c363f67dfb47cf62171f53250b18
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 23:15:42 -0800

    added unit test for get row after row has been modified

--- A3/test_get_row.py
+++ A3/test_get_row.py
@@ -1,10 +1,15 @@
 from unittest import TestCase
 from rebel import get_row
+import rebel
 
 
 class TestGetRow(TestCase):
     def test_get_row_before_game_play(self):
-        self.assertIs(get_row(), 5)
+        self.assertIs(get_row(), rebel.rebel["Row"])
+
+    def test_get_row_after_modified(self):
+        rebel.set_row(1)
+        self.assertIs(get_row(), rebel.rebel["Row"])
 
     def test_get_row_return_type(self):
         self.assertIsInstance(get_row(), int)
