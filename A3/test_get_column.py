bac953fcf101524cb5017f4d9bff8b1f1ee836a2
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 23:18:47 -0800

    added 2 unit tests for get column

--- A3/test_get_column.py
+++ A3/test_get_column.py
@@ -1,7 +1,15 @@
 from unittest import TestCase
 from rebel import get_column
+import rebel
 
 
 class TestGetColumn(TestCase):
     def test_get_column_before_game_play(self):
-        self.assertEqual(get_column(), 5)
+        self.assertIs(get_column(), rebel.rebel["Column"])
+
+    def test_get_column_after_modified(self):
+        rebel.set_column(3)
+        self.assertIs(get_column(), rebel.rebel["Column"])
+
+    def test_get_column_return_type(self):
+        self.assertIsInstance(get_column(), int)
