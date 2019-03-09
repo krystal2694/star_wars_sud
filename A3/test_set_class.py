b0966f82cd2299d72e4f0b840655362027f3dff4
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 23:37:20 -0800

    added unit test to test that value has changed from original

--- A3/test_set_class.py
+++ A3/test_set_class.py
@@ -1,9 +1,14 @@
 from unittest import TestCase
 from rebel import set_class
-from rebel import rebel
+import rebel
 
 
 class TestSetClass(TestCase):
-    def test_set_class(self):
+    def test_set_class_value_reflects_parameter_passed(self):
         set_class("Jedi")
-        self.assertEqual(rebel["Class"], "Jedi")
+        self.assertEqual(rebel.rebel["Class"], "Jedi")
+
+    def test_set_class_changed_from_original(self):
+        original = rebel.rebel["Class"]
+        set_class("Rebel Fighter")
+        self.assertIsNot(original, rebel.rebel["Class"])
