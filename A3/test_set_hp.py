21c95391e7526148e3a4dc4eda2f9c7b11fcf6de
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 21:24:08 -0800

    Modified import statements

--- A3/test_set_hp.py
+++ A3/test_set_hp.py
@@ -1,16 +1,17 @@
 from unittest import TestCase
-import rebel
+from rebel import set_hp
+from rebel import rebel
 
 
 class TestSetHp(TestCase):
     def test_set_hp_with_0(self):
-        rebel.set_hp(0)
-        self.assertEqual(rebel.rebel["HP"], 0)
+        set_hp(0)
+        self.assertEqual(rebel["HP"], 0)
 
     def test_set_hp_with_positive_int(self):
-        rebel.set_hp(8)
-        self.assertEqual(rebel.rebel["HP"], 8)
+        set_hp(8)
+        self.assertEqual(rebel["HP"], 8)
 
     def test_set_hp_with_negative_int(self):
-        rebel.set_hp(-12)
-        self.assertEqual(rebel.rebel["HP"], -12)
+        set_hp(-12)
+        self.assertEqual(rebel["HP"], -12)
