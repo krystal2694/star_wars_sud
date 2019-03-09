d2972226616d9e11ab6acc3d5779932fe0368c82
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 23:12:45 -0800

    added unit test to prove name changed from original

--- A3/test_set_name.py
+++ A3/test_set_name.py
@@ -6,6 +6,12 @@ from unittest.mock import patch
 
 class TestSetName(TestCase):
     @patch('builtins.input', return_value="Yoda")
-    def test_set_name(self, mock_input):
+    def test_set_name_reflects_user_input(self, mock_input):
         set_name()
         self.assertEqual(rebel["Name"], "Yoda")
+
+    @patch('builtins.input', return_value="Chewie")
+    def test_set_name_changed_from_original(self, mock_input):
+        original = rebel["Name"]
+        set_name()
+        self.assertIsNot(original, rebel["Name"])
