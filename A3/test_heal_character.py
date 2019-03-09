0a67b3d86ce4936eed3eae03b800b3ceda9629da
krystal2694 <pwong2632my.bcit.ca>
2019-03-09 11:39:11 -0800

    Modified unit tests to reference character dict instead of calling other functions

--- A3/test_heal_character.py
+++ A3/test_heal_character.py
@@ -1,10 +1,29 @@
 from unittest import TestCase
+from unittest.mock import patch
 from sud import heal_character
 from rebel import rebel
+import io
 
 
 class TestHealCharacter(TestCase):
     def test_heal_character_when_hp_less_than_10(self):
         for i in range(1, 10):
             rebel["HP"] = i
-            self.fail()
+            hp_before_heal = rebel["HP"]
+            heal_character()
+            self.assertEqual(rebel["HP"], hp_before_heal + 1)
+
+    def test_heal_character_when_hp_less_than_10_print_output(self):
+        for i in range(1, 10):
+            rebel["HP"] = i
+            hp_before_heal = rebel["HP"]
+            heal_character()
+            self.assertEqual(rebel["HP"], hp_before_heal + 1)
+
+    def test_heal_character_when_at_full_health(self):
+        rebel["HP"] = 10
+        hp_before_heal = rebel["HP"]
+        heal_character()
+        self.assertEqual(rebel["HP"], hp_before_heal)
+
+
