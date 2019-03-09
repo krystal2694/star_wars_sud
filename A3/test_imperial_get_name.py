5c7de8306c35306f0b60e455933aaf27bed24e2a
krystal2694 <pwong2632my.bcit.ca>
2019-03-08 23:44:14 -0800

    added unit test for type error

--- A3/test_imperial_get_name.py
+++ A3/test_imperial_get_name.py
@@ -11,3 +11,8 @@ class TestGetName(TestCase):
     def test_get_name_return_type(self):
         for i in range(len(imperial_forces)):
             self.assertIsInstance(get_name(i), str)
+
+    def test_get_name_with_non_int_objects(self):
+        with self.assertRaises(TypeError):
+            get_name("")
+            get_name(2.3)
