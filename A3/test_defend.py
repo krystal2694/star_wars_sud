d696a1fa725d25306f5bc2fa0e06203c8fa80f55
krystal2694 <pwong2632my.bcit.ca>
2019-03-11 23:27:23 -0700

    Reversed order of side effects for patch randint since I changed the order of the code in function

--- A3/test_defend.py
+++ A3/test_defend.py
@@ -7,7 +7,7 @@ import io
 
 class TestDefend(TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
-    @patch('battle.randint', side_effect=[3, 1])
+    @patch('battle.randint', side_effect=[1, 3])
     def test_defend_print_output_when_miss(self, mock_randint, mock_stdout):
         expected_output = "The Imperial Spy strikes!\n" \
                           "You evaded the attack!\n\n"
@@ -15,7 +15,7 @@ class TestDefend(TestCase):
         self.assertEqual(expected_output, mock_stdout.getvalue())
 
     @patch('sys.stdout', new_callable=io.StringIO)
-    @patch('battle.randint', side_effect=[4, 13])
+    @patch('battle.randint', side_effect=[13, 4])
     def test_defend_print_output_when_hit_and_character_survives(self, mock_randint, mock_stdout):
         expected_output = "The Sith Lord strikes!\n" \
                           "You have taken a 4 point hit!\n" \
@@ -25,7 +25,7 @@ class TestDefend(TestCase):
         self.assertEqual(expected_output, mock_stdout.getvalue())
 
     @patch('sys.stdout', new_callable=io.StringIO)
-    @patch('battle.randint', side_effect=[6, 14])
+    @patch('battle.randint', side_effect=[14, 6])
     def test_defend_print_output_when_hit_and_character_dies(self, mock_randint, mock_stdout):
         expected_output = "The AT-AT Walker strikes!\n" \
                           "You have taken a 6 point hit!\n" \
@@ -35,13 +35,13 @@ class TestDefend(TestCase):
         defend(6)
         self.assertEqual(expected_output, mock_stdout.getvalue())
 
-    @patch('battle.randint', side_effect=[2, 15])
+    @patch('battle.randint', side_effect=[15, 2])
     def test_defend_when_hit_damage_is_reflected_in_new_hp(self, mock_randint):
         hp_before_attack = rebel["HP"]
         defend(0)
         self.assertEqual(rebel["HP"], hp_before_attack - 2)
 
-    @patch('battle.randint', side_effect=[3, 4])
+    @patch('battle.randint', side_effect=[4, 3])
     def test_defend_when_miss_hp_is_unchanged(self, mock_randint):
         hp_before_attack = rebel["HP"]
         defend(1)
