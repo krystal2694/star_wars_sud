b41634737d4d648ce107e9cdeeb1c4c1c5c15190
krystal2694 <pwong2632my.bcit.ca>
2019-03-11 22:17:23 -0700

    removed unnecessary unit tests, added unit test for return type

--- A3/test_roll_for_enemy.py
+++ A3/test_roll_for_enemy.py
@@ -5,27 +5,13 @@ from sud import roll_for_enemy
 
 class TestRollForEnemy(TestCase):
     @patch('sud.randint', return_value=1)
-    def test_roll_for_enemy_with_int_1(self, mock_randint):
+    def test_roll_for_enemy_return_true(self, mock_randint):
         self.assertTrue(roll_for_enemy())
 
-    @patch('sud.randint', return_value=2)
-    def test_roll_for_enemy_with_int_2(self, mock_randint):
-        self.assertFalse(roll_for_enemy())
-
     @patch('sud.randint', return_value=3)
-    def test_roll_for_enemy_with_int_3(self, mock_randint):
-        self.assertFalse(roll_for_enemy())
-
-    @patch('sud.randint', return_value=4)
-    def test_roll_for_enemy_with_int_4(self, mock_randint):
-        self.assertFalse(roll_for_enemy())
-
-    @patch('sud.randint', return_value=5)
-    def test_roll_for_enemy_with_int_5(self, mock_randint):
+    def test_roll_for_enemy_return_false(self, mock_randint):
         self.assertFalse(roll_for_enemy())
 
     @patch('sud.randint', return_value=6)
-    def test_roll_for_enemy_with_int_6(self, mock_randint):
-        self.assertFalse(roll_for_enemy())
-
-
+    def test_roll_for_enemy_return_type(self, mock_randint):
+        self.assertIsInstance(roll_for_enemy(), bool)
