7c28df89c29e64091fa63a6bcbfcdb5b957fe9b4
krystal2694 <pwong2632my.bcit.ca>
2019-03-11 23:54:55 -0700

    Improved unit test names and modified tests to reference character dictionary instead of calling another function

--- A3/test_set_row.py
+++ A3/test_set_row.py
@@ -1,14 +1,14 @@
 from unittest import TestCase
 from rebel import set_row
-import rebel
+from rebel import rebel
 
 
 class TestSetRow(TestCase):
-    def test_set_row_value_reflects_parameter_passed(self):
+    def test_set_row_value_reflects_argument_passed(self):
         set_row(4)
-        self.assertEqual(rebel.rebel["Row"], 4)
+        self.assertEqual(rebel["Row"], 4)
 
     def test_set_row_changed_from_original(self):
-        original = rebel.get_row()
+        original = rebel["Row"]
         set_row(6)
-        self.assertIsNot(original, rebel.get_row())
+        self.assertIsNot(original, rebel["Row"])
