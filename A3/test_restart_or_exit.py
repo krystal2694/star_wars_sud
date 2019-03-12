6482424dc3bf581f97d51013e27339755412e5f5
krystal2694 <pwong2632my.bcit.ca>
2019-03-11 22:43:15 -0700

    Added set up function to set character hp to 0 before each unit test

--- A3/test_restart_or_exit.py
+++ A3/test_restart_or_exit.py
@@ -6,10 +6,13 @@ import io
 
 
 class TestRestartOrExit(TestCase):
+    # set hp to 0 for each unit test so the function asks the user if they would like to play again
+    def setUp(self):
+        rebel["HP"] = 0
+
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=["w", "3", "no", "y"])
     def test_restart_or_exit_ask_user_input_until_input_is_valid(self, mock_input, mock_stdout):
-        rebel["HP"] = 0
         expected_output = "\n" \
                           "✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ \n" \
                           "✨                                 ✨\n" \
@@ -30,7 +33,6 @@ class TestRestartOrExit(TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', return_value="y")
     def test_restart_or_exit_with_user_input_y(self, mock_input, mock_stdout):
-        rebel["HP"] = -1
         expected_output = "\n" \
                           "✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ ✨ \n" \
                           "✨                                 ✨\n" \
@@ -50,6 +52,5 @@ class TestRestartOrExit(TestCase):
 
     @patch('builtins.input', return_value="n")
     def test_restart_or_exit_with_user_input_n(self, mock_input):
-        rebel["HP"] = -2
         with self.assertRaises(SystemExit):
             restart_or_exit()
