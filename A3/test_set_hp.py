5c5f0dc8c0d2f3538464ccc99a99fba058776123
krystal2694 <pwong2632my.bcit.ca>
2019-03-11 23:53:09 -0700

    Added set up function to reset hp to 10 before each unit test

--- A3/test_set_hp.py
+++ A3/test_set_hp.py
@@ -1,22 +1,23 @@
 from unittest import TestCase
 from rebel import set_hp
-import rebel
+from rebel import rebel
 
 
 class TestSetHp(TestCase):
+    def setUp(self):
+        """Reset character HP to 10 before each unit test."""
+        # This makes it clear what the value of their HP is before the function is called
+        rebel["HP"] = 10
+
     def test_set_hp_with_0(self):
         set_hp(0)
-        self.assertEqual(rebel.rebel["HP"], 0)
+        self.assertEqual(rebel["HP"], 0)
 
     def test_set_hp_with_positive_int(self):
         set_hp(8)
-        self.assertEqual(rebel.rebel["HP"], 8)
-
-    def test_set_hp_with_negative_int(self):
-        set_hp(-12)
-        self.assertEqual(rebel.rebel["HP"], -12)
+        self.assertEqual(rebel["HP"], 8)
 
     def test_set_hp_changed_from_original(self):
-        original = rebel.get_hp()
+        original = rebel["HP"]
         set_hp(4)
-        self.assertIsNot(original, rebel.rebel["HP"])
+        self.assertNotEqual(original, rebel["HP"])
