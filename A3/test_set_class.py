00fd07c95909f6843e880c142970a4b7539f4e6d
krystal2694 <pwong2632my.bcit.ca>
2019-03-11 23:51:37 -0700

    Improved unit test names

--- A3/test_set_class.py
+++ A3/test_set_class.py
@@ -1,14 +1,14 @@
 from unittest import TestCase
 from rebel import set_class
-import rebel
+from rebel import rebel
 
 
 class TestSetClass(TestCase):
-    def test_set_class_value_reflects_parameter_passed(self):
+    def test_set_class_value_reflects_argument_passed(self):
         set_class("Jedi")
-        self.assertEqual(rebel.rebel["Class"], "Jedi")
+        self.assertEqual(rebel["Class"], "Jedi")
 
     def test_set_class_changed_from_original(self):
-        original = rebel.rebel["Class"]
+        original = rebel["Class"]
         set_class("Rebel Fighter")
-        self.assertIsNot(original, rebel.rebel["Class"])
+        self.assertIsNot(original, rebel["Class"])
