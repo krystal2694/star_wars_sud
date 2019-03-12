309f37bd2d8f3f5b0fad4d50ee3c3d09fe29b880
krystal2694 <pwong2632my.bcit.ca>
2019-03-11 23:45:28 -0700

    Added post conditions to functions in imperial module

--- A3/imperial.py
+++ A3/imperial.py
@@ -14,6 +14,7 @@ def get_name(index: int)-> str:
     """Return imperial force name.
 
     PRECONDITION: index must be an int representing an existing index in imperial_forces
+    POSTCONDITION: Return name of imperial force according to index
     >>> get_name(3)
     'Bounty Hunter'
     >>> get_name(6)
@@ -26,6 +27,8 @@ def get_hp(index: int)-> int:
     """Return imperial force HP.
 
     PRECONDITION: index must be an int representing an existing index in imperial_forces
+    POSTCONDITION: Return HP of imperial force according to index
+
     >>> get_hp(5)
     5
     >>> get_hp(0)
@@ -37,7 +40,8 @@ def get_hp(index: int)-> int:
 def set_hp(index: int, new_hp: int)-> None:
     """Modify imperial force HP.
 
-    PRECONDITION: index must be an int representing an existing index in imperial_forces"""
+    PRECONDITION: index must be an int representing an existing index in imperial_forces
+    POSTCONDITION: Modify HP of imperial force according to index"""
     global imperial_forces
     imperial_forces[index]["HP"] = new_hp
 
@@ -46,6 +50,8 @@ def get_dexterity(index: int)-> int:
     """Return imperial force dexterity.
 
     PRECONDITION: index must be an int representing an existing index in imperial_forces
+    POSTCONDITION: Return dexterity of imperial force according to index
+
     >>> get_dexterity(0)
     2
     >>> get_dexterity(4)
