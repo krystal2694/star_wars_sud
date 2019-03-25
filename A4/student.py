000039d9c2b81e049873988ade1c90da8f1a808d
krystal2694 <pwong2632my.bcit.ca>
2019-03-24 21:04:43 -0700

    basic class init created

--- A4/student.py
+++ A4/student.py
@@ -1,2 +1,17 @@
 class Student:
     """A class representing a student."""
+
+    def __init__(self, first_name: str, last_name: str, student_num: str, status: str):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.student_num = student_num
+        self.status = status
+        self.final_grades = []
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
