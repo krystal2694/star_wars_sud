28a0f7857f27340e5aaa53b6e05e8d802b8e2efe
krystal2694 <pwong2632my.bcit.ca>
2019-03-24 21:57:13 -0700

    raise value error when student number not in right format

--- A4/student.py
+++ A4/student.py
@@ -1,16 +1,19 @@
 class Student:
     """A class representing a student."""
 
-    def __init__(self, first_name: str, last_name: str, student_num: str, status: str):
-        self.first_name = first_name.title()
-        self.last_name = last_name.title()
-        self.student_num = student_num
+    def __init__(self, first_name: str, last_name: str, student_num: str, status: bool):
+        self.first_name = first_name
+        self.last_name = last_name
+        if len(student_num) != 9 or student_num[0] != "A":
+            raise ValueError("The student number must be in the format A12345678!")
+        else:
+            self.student_num = student_num
         self.status = status
         self.final_grades = []
 
 
 def main():
-    pass
+    Student("Krystal", "Wong", "A01089672", True)
 
 
 if __name__ == '__main__':
