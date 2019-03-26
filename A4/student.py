014c6fed270a9463d331dda704bb9ef8bb30a351
krystal2694 <pwong2632my.bcit.ca>
2019-03-25 17:05:24 -0700

    added get grades string function

--- A4/student.py
+++ A4/student.py
@@ -3,24 +3,31 @@ class Student:
 
     def __init__(self, first_name: str, last_name: str, student_num: str, status: bool, final_grades: list):
         if len(first_name.split()) == 0:
-            raise ValueError("First name cannot be blank!")
+            print("First name cannot be blank!")
+            raise ValueError
         else:
             self.first_name = first_name.title()
         if len(last_name.split()) == 0:
-            raise ValueError("Last name cannot be blank!")
+            print("Last name cannot be blank!")
+            raise ValueError
         self.last_name = last_name.title()
         if len(student_num) == 9 and student_num[0] == "A" and student_num[1:].isdigit():
             self.student_num = student_num
         else:
-            raise ValueError("The student number must be the character A, followed by 8 digits!")
-
+            print("The student number must be the character A, followed by 8 digits!")
+            raise ValueError
         self.status = status
         self.final_grades = final_grades
 
+    def get_grades_string(self):
+        grades = ""
+        for grade in self.final_grades:
+            grades += str(grade) + " "
+        return grades
+
 
 def main():
-    krystal = Student("Krystal", "Wong", "A01089662", True, [])
-    print(krystal.final_grades)
+    krystal = Student("Krystal", "Wong", "A01089662", True, [94, 95, 94, 99])
 
 
 if __name__ == '__main__':
