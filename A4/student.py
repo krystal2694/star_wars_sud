b065362068f86e5c60d3d2e8f055c77b5f85fc93
krystal2694 <pwong2632my.bcit.ca>
2019-03-25 20:29:09 -0700

    modified initialization for first and last name to check if characters are alpha

--- A4/student.py
+++ A4/student.py
@@ -2,16 +2,17 @@ class Student:
     """A class representing a student."""
 
     def __init__(self, first_name: str, last_name: str, student_num: str, status: bool, final_grades: list):
-        if len(first_name.split()) == 0:
-            print("First name cannot be blank!")
-            raise ValueError
-        else:
+        if first_name.isalpha():
             self.first_name = first_name.title()
-
-        if len(last_name.split()) == 0:
-            print("Last name cannot be blank!")
+        else:
+            print("First name must only be made up of alphabetical characters!")
             raise ValueError
-        self.last_name = last_name.title()
+
+        if last_name.isalpha():
+            self.last_name = last_name.title()
+        else:
+            print("Last name must only be made up of alphabetical characters!")
+            raise ValueError
 
         if len(student_num) == 9 and student_num[0] == "A" and student_num[1:].isdigit():
             self.student_num = student_num
@@ -31,16 +32,16 @@ class Student:
     def get_grades(self):
         grades = ""
         for grade in self.final_grades:
-            grades += str(grade) + " "
+            grades += grade + " "
         return grades
 
     def get_info(self):
-        return (self.first_name + " " + self.last_name + " " + self.student_num + " " + str(self.status)
-                + " " + self.get_grades())
+        return self.first_name + " " + self.last_name + " " + self.student_num + " " + str(self.status) \
+               + " " + self.get_grades()
 
 
 def main():
-    krystal = Student("Krystal", "Wong", "A01089662", True, [94, 95, 89, 99])
+    krystal = Student("Krystal", "Wong", "A01089662", True, ["94", "95", "89", "99"])
     print(krystal.get_info())
 
 
