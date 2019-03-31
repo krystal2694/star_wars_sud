430722bf3c065a24fb53e8f5e06d466cd390498f
krystal2694 <pwong2632my.bcit.ca>
2019-03-30 18:19:36 -0700

    completed unit tests for get_last_name function

---
+++ A4/test_student_get_last_name.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from student import Student
+
+mock_student = Student("Karen", "Perry", "A45312546", "False", "65", "50")
+
+
+class TestStudent(TestCase):
+    def test_get_last_name_return_type(self):
+        self.assertIsInstance(mock_student.get_first_name(), str)
+
+    def test_get_last_name_return_correct_value(self):
+        self.assertEqual(mock_student.get_last_name(), "Perry")
