51fae901c2f00ddab5e2425d1a3602cd125d02de
krystal2694 <pwong2632my.bcit.ca>
2019-03-30 17:24:33 -0700

    refactored student class attributes to be private

--- A4/student.py
+++ A4/student.py
@@ -3,41 +3,42 @@ class Student:
 
     def __init__(self, first_name: str, last_name: str, student_num: str, status: str, *final_grades: str)-> None:
         """Initialize student attributes."""
-        self.last_name = None
-        self.status = None
+        self.__last_name = None
+        self.__status = None
         self.set_last_name(last_name)
         self.set_status(status)
 
         if first_name.isalpha() is False:
-            raise ValueError("First name must only be made up of alphabetical characters!")
-        self.first_name = first_name.title()
+            raise ValueError("First name must only be made up of alphabetical characters!\nStudent could not be added.")
+        self.__first_name = first_name.title()
 
         if len(student_num) != 9 or student_num[0] != "A" or student_num[1:].isdigit() is False:
-            raise ValueError("The student number must be the character A, followed by 8 digits!")
+            raise ValueError("The student number must be the character A, followed by 8 digits!"
+                             "\nStudent could not be added.")
         self.__student_num = student_num
 
-        self.final_grades = []
+        self.__final_grades = []
         for grade in final_grades:
             if int(grade) < 0 or int(grade) > 100:
                 raise ValueError("A grade cannot be less than 0 or greater than 100!")
-            self.final_grades.append(grade)
+            self.__final_grades.append(grade)
 
     def __str__(self)-> str:
-        return self.first_name + " " + self.last_name + " " + self.__student_num + " " + self.status \
-               + " " + " ".join(self.final_grades)
+        return self.__first_name + " " + self.__last_name + " " + self.__student_num + " " + self.__status \
+               + " " + " ".join(self.__final_grades)
 
     def get_first_name(self)-> str:
         """Return first name of student."""
-        return self.first_name
+        return self.__first_name
 
     def get_last_name(self)-> str:
         """Return last name of student."""
-        return self.last_name
+        return self.__last_name
 
     def set_last_name(self, last_name: str)-> None:
         """Set last name of student."""
         if last_name.isalpha():
-            self.last_name = last_name.title()
+            self.__last_name = last_name.title()
         else:
             raise ValueError("Last name must only be made up of alphabetical characters!")
 
@@ -47,32 +48,30 @@ class Student:
 
     def get_final_grades(self)-> list:
         """Return list of student's final grades."""
-        return self.final_grades
+        return self.__final_grades
 
     def get_status(self)-> list:
         """Return status of student."""
-        return self.status
+        return self.__status
 
     def set_status(self, status: str)-> None:
         """Set status of student."""
 
         if status != "True" and status != "False":
             raise ValueError("Status can only be 'True' or 'False'.")
-        self.status = status
+        self.__status = status
 
     def add_final_grade(self, new_grade: str)-> None:
         """Add grade to list of student's final grades."""
 
         if int(new_grade) < 0 or int(new_grade) > 100:
             raise ValueError("A grade cannot be less than 0 or greater than 100!")
-        self.final_grades.append(new_grade)
+        self.__final_grades.append(new_grade)
 
 
 def main():
     krystal = Student("Krystal", "Wong", "A01089662", "True", "94", "95", "89", "99")
     print(krystal)
-    # krystal.add_final_grade("100")
-    # print(krystal.get_info())
 
 
 if __name__ == '__main__':
