ae04523f900130b8a303dd595a1e7e102ef09d3e
krystal2694 <pwong2632my.bcit.ca>
2019-03-30 19:04:11 -0700

    completed unit tests for add_final_grade function

---
+++ A4/test_student_add_final_grade.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from student import Student
+
+
+class TestStudent(TestCase):
+    def test_add_final_grade_successful(self):
+        mock_student = Student("Jenny", "Kramer", "A42154852", "True", "89", "76")
+        mock_student.add_final_grade("85")
+        self.assertEqual(mock_student.get_final_grades(), ["89", "76", "85"])
+
+    def test_add_final_grade_with_num_greater_than_100(self):
+        mock_student = Student("Jenny", "Kramer", "A42154852", "True", "89", "76")
+        with self.assertRaises(ValueError):
+            mock_student.add_final_grade("150")
+
+    def test_add_final_grade_with_negative_num(self):
+        mock_student = Student("Jenny", "Kramer", "A42154852", "True", "89", "76")
+        with self.assertRaises(ValueError):
+            mock_student.add_final_grade("-20")
+
