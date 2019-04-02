595f44e5158cf7cb5b6d64b7f911a3e7bff4e1f6
krystal2694 <pwong2632my.bcit.ca>
2019-04-02 16:47:46 -0700

    modified unit tests after modifying student class

--- A4/test_file_write.py
+++ A4/test_file_write.py
@@ -8,16 +8,16 @@ from unittest.mock import patch
 class TestFileWrite(TestCase):
     def test_file_write_successful(self):
         with patch('builtins.open', mock_open(read_data="")) as mock_file:
-            mock_student = Student("Bruce", "Banner", "A12145854", "False", "54", "75")
+            mock_student = Student("Bruce", "Banner", "A12145854", False, 54, 75)
             file_write(mock_student)
             mock_file().write.assert_called_once_with("Bruce Banner A12145854 False 54 75\n")
 
     def test_file_write_successful_returns_true(self):
         with patch('builtins.open', mock_open(read_data="Bruce Banner A12145854 False 54 75")):
-            mock_student = Student("Bruce", "Banner", "A12145854", "False", "54", "75")
+            mock_student = Student("Bruce", "Banner", "A12145854", False, 54, 75)
             self.assertTrue(file_write(mock_student))
 
     def test_file_write_return_type(self):
         with patch('builtins.open', mock_open(read_data="Bruce Banner A12145854 False 54 75")):
-            mock_student = Student("Bruce", "Banner", "A12145854", "False", "54", "75")
+            mock_student = Student("Bruce", "Banner", "A12145854", False, 54, 75)
             self.assertIsInstance(file_write(mock_student), bool)
