d4c9f03e225f84acb376a4b3190373eab6c08891
krystal2694 <pwong2632my.bcit.ca>
2019-04-02 16:35:29 -0700

    fixed unit test after changing grades from str to int

--- A4/test_collect_student_info.py
+++ A4/test_collect_student_info.py
@@ -6,7 +6,7 @@ import io
 
 class TestCollectStudentInfo(TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
-    @patch('builtins.input', side_effect=["Peter", "Parker", "A21487956", "True", "85", "done"])
+    @patch('builtins.input', side_effect=["Peter", "Parker", "A21487956", "Y", "85", "done"])
     def test_collect_student_info_printed_output(self, mock_input, mock_stdout):
         expected = "\nPlease enter the students' information.\n" \
                    "Enter the student's final grades.\n" \
@@ -14,13 +14,13 @@ class TestCollectStudentInfo(TestCase):
         collect_student_info()
         self.assertEqual(mock_stdout.getvalue(), expected)
 
-    @patch('builtins.input', side_effect=["Peter", "Parker", "A21487956", "True", "85", "done"])
+    @patch('builtins.input', side_effect=["Peter", "Parker", "A21487956", "Y", "85", "done"])
     def test_collect_student_info_return_type(self, mock_input):
         self.assertIsInstance(collect_student_info(), list)
 
-    @patch('builtins.input', side_effect=["Peter", "Parker", "A21487956", "True", "85", "done"])
+    @patch('builtins.input', side_effect=["Peter", "Parker", "A21487956", "Y", "85", "done"])
     def test_collect_student_info_values_reflect_user_input(self, mock_input):
-        user_input = ["Peter", "Parker", "A21487956", "True", ["85"]]
+        user_input = ["Peter", "Parker", "A21487956", True, [85]]
         student_info = collect_student_info()
         for i in range(len(user_input)):
             self.assertEqual(user_input[i], student_info[i])
