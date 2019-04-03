69b779207f1e8235adc5e1b83c74ef0f29f16fc4
krystal2694 <pwong2632my.bcit.ca>
2019-04-02 17:23:52 -0700

    modified unit tests after modifying student class

--- A4/test_student_set_status.py
+++ A4/test_student_set_status.py
@@ -4,11 +4,11 @@ from student import Student
 
 class TestStudent(TestCase):
     def test_set_status_reflects_argument_passed(self):
-        mock_student = Student("Jenny", "Kramer", "A42154852", "True", "89", "76")
-        mock_student.set_status("False")
-        self.assertEqual(mock_student.get_status(), "False")
+        mock_student = Student("Jenny", "Kramer", "A42154852", True, 89, 76)
+        mock_student.set_status(False)
+        self.assertEqual(mock_student.get_status(), False)
 
-    def test_set_status_with_invalid_value(self):
-        mock_student = Student("Jenny", "Kramer", "A42154852", "True", "89", "76")
-        with self.assertRaises(ValueError):
-            mock_student.set_status("Good")
+    def test_set_status_type(self):
+        mock_student = Student("Jenny", "Kramer", "A42154852", False, 89, 76)
+        mock_student.set_status(False)
+        self.assertIsInstance(mock_student.get_status(), bool)
