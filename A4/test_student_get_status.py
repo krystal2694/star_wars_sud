5ecaa2770cb1fdd14ba3ca3f89b916b940704a5e
krystal2694 <pwong2632my.bcit.ca>
2019-04-02 17:20:07 -0700

    modified unit tests after modifying student class

--- A4/test_student_get_status.py
+++ A4/test_student_get_status.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 from student import Student
 
-mock_student = Student("Karen", "Perry", "A45312546", "False", "65", "50")
+mock_student = Student("Karen", "Perry", "A45312546", False, 65, 50)
 
 
 class TestStudent(TestCase):
     def test_get_status_return_type(self):
-        self.assertIsInstance(mock_student.get_status(), str)
+        self.assertIsInstance(mock_student.get_status(), bool)
 
     def test_get_status_return_correct_value(self):
-        self.assertEqual(mock_student.get_status(), "False")
+        self.assertEqual(mock_student.get_status(), False)
