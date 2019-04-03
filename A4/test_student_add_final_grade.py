b91571192518a84fa33c52117ffa59afb9bb4b3e
krystal2694 <pwong2632my.bcit.ca>
2019-04-02 17:16:55 -0700

    modified unit tests after modifying student class

--- A4/test_student_add_final_grade.py
+++ A4/test_student_add_final_grade.py
@@ -4,17 +4,17 @@ from student import Student
 
 class TestStudent(TestCase):
     def test_add_final_grade_successful(self):
-        mock_student = Student("Jenny", "Kramer", "A42154852", "True", "89", "76")
-        mock_student.add_final_grade("85")
-        self.assertEqual(mock_student.get_final_grades(), ["89", "76", "85"])
+        mock_student = Student("Jenny", "Kramer", "A42154852", True, 89, 76)
+        mock_student.add_final_grade(85)
+        self.assertEqual(repr(mock_student).split()[4:7], ["89", "76", "85"])
 
     def test_add_final_grade_with_num_greater_than_100(self):
-        mock_student = Student("Jenny", "Kramer", "A42154852", "True", "89", "76")
+        mock_student = Student("Jenny", "Kramer", "A42154852", True, 89, 76)
         with self.assertRaises(ValueError):
-            mock_student.add_final_grade("150")
+            mock_student.add_final_grade(150)
 
     def test_add_final_grade_with_negative_num(self):
-        mock_student = Student("Jenny", "Kramer", "A42154852", "True", "89", "76")
+        mock_student = Student("Jenny", "Kramer", "A42154852", True, 89, 76)
         with self.assertRaises(ValueError):
-            mock_student.add_final_grade("-20")
+            mock_student.add_final_grade(-20)
 
