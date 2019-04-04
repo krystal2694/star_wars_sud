8bb31d2cb904085a9e3042ff762104b49f6a1ab9
krystal2694 <pwong2632my.bcit.ca>
2019-04-03 21:39:59 -0700

    formatted unit tests for get_student_num

--- A4/test_student_get_student_num.py
+++ A4/test_student_get_student_num.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 from student import Student
 
-mock_student = Student("Karen", "Perry", "A45312546", False, 65, 50)
-
 
 class TestStudent(TestCase):
     def test_get_student_num_return_type(self):
+        mock_student = Student("Karen", "Perry", "A45312546", False, 65, 50)
         self.assertIsInstance(mock_student.get_student_num(), str)
 
     def test_get_student_num_return_correct_value(self):
+        mock_student = Student("Karen", "Perry", "A45312546", False, 65, 50)
         self.assertEqual(mock_student.get_student_num(), "A45312546")
