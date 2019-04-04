7bf6da7f2838c5c52b3738c726b23d47715b4057
krystal2694 <pwong2632my.bcit.ca>
2019-04-03 21:40:35 -0700

    formatted unit tests for __str__

--- A4/test_student_str.py
+++ A4/test_student_str.py
@@ -3,15 +3,15 @@ from student import Student
 from unittest.mock import patch
 import io
 
-mock_student = Student("Jordan", "Jenkins", "A45687563", True, 89, 91, 85)
-
 
 class TestStudent(TestCase):
     def test___str__return_type(self):
+        mock_student = Student("Jordan", "Jenkins", "A45687563", True, 89, 91, 85)
         self.assertIsInstance(str(mock_student), str)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test___str__return_correct_values(self, mock_stdout):
+        mock_student = Student("Jordan", "Jenkins", "A45687563", True, 89, 91, 85)
         expected = "Name: Jordan Jenkins, Student Number: A45687563, In Good Standing: True, Grades: 89 91 85\n"
         print(mock_student)
         self.assertEqual(expected, mock_stdout.getvalue())
