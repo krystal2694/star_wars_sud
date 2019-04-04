66facb5d51b1605c1b2da43230c03765d7a70812
krystal2694 <pwong2632my.bcit.ca>
2019-04-03 21:46:14 -0700

    removed return type unit test for __str__ (redundant)

--- A4/test_student_str.py
+++ A4/test_student_str.py
@@ -5,10 +5,6 @@ import io
 
 
 class TestStudent(TestCase):
-    def test___str__return_type(self):
-        mock_student = Student("Jordan", "Jenkins", "A45687563", True, 89, 91, 85)
-        self.assertIsInstance(str(mock_student), str)
-
     @patch('sys.stdout', new_callable=io.StringIO)
     def test___str__return_correct_values(self, mock_stdout):
         mock_student = Student("Jordan", "Jenkins", "A45687563", True, 89, 91, 85)
