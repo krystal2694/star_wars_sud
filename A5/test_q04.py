a1a81f050c61de2a59c250484006e223fc4337da
krystal2694 <pwong2632my.bcit.ca>
2019-04-05 20:37:57 -0700

    completed unit tests for q04

---
+++ A5/test_q04.py
@@ -0,0 +1,59 @@
+from unittest import TestCase
+from q04 import is_all_numbers
+from q04 import is_all_strings
+from q04 import selection_sort
+from q04 import sort_items
+
+
+class TestIsAllNumbers(TestCase):
+    def test_all_numbers_return_type(self):
+        self.assertIsInstance(is_all_numbers([-3, 0, 78, 45]), bool)
+
+    def test_is_all_numbers_return_true_with_all_integers(self):
+        self.assertTrue(is_all_numbers([-3, 0, 78, 45]))
+
+    def test_is_all_numbers_return_true_with_some_floats(self):
+        self.assertTrue(is_all_numbers([-5.5, -3, 0, 3.14, 2]))
+
+    def test_is_all_numbers_return_true_all_floats(self):
+        self.assertTrue(is_all_numbers([-2.1, 0.1, -0.9, 5.7]))
+
+    def test_all_numbers_return_false(self):
+        self.assertFalse(is_all_numbers([-0.3, '45', 3.14, -80]))
+
+
+class TestIsAllStrings(TestCase):
+    def test_is_all_strings_return_type(self):
+        self.assertIsInstance(is_all_strings(['a', 1, '4', 23]), bool)
+
+    def test_is_all_string_yes(self):
+        self.assertTrue(is_all_strings(['a', 'the', 'hello', '54']))
+
+    def test_is_all_string_no(self):
+        self.assertFalse(is_all_strings(['a', 'the', 'hello', 54]))
+
+
+class TestSelectionSort(TestCase):
+    def test_selection_sort_empty_list(self):
+        with self.assertRaises(ValueError):
+            selection_sort([])
+
+    def test_selection_sort_non_sortable_items(self):
+        with self.assertRaises(ValueError):
+            selection_sort(['a', 2, 3.14, 'the'])
+
+
+class TestSortItems(TestCase):
+    def test_sort_items_with_str_items_sorted(self):
+        sorted_list = sort_items(0, ['a', 'bee', 'the', 'me', 'car', 'me'])
+        for i in range(len(sorted_list) - 1):
+            self.assertTrue(sorted_list[i] <= sorted_list[i + 1])
+
+    def test_sort_items_with_numbers_items_sorted(self):
+        sorted_list = sort_items(0, [5, 2, 1, 9, 2, 10])
+        for i in range(len(sorted_list) - 1):
+            self.assertTrue(sorted_list[i] <= sorted_list[i + 1])
+
+    def test_sort_items_return_type(self):
+        self.assertIsInstance(sort_items(0, [5, 2, 1, 9, 2, 10]), list)
+
