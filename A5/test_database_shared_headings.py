99ed543e4e9fd118323c6ec8d088df9a183e63c0
krystal2694 <pwong2632my.bcit.ca>
2019-04-08 20:37:32 -0700

    completed unit tests for q10

---
+++ A5/test_database_shared_headings.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from q10 import database_shared_headings
+
+
+superheroes = {'Captain America':
+               {'name': 'Steve Rogers',
+                'superpower': 'super strength',
+                'born': 1918,
+                'member of Avengers': True},
+               'Iron Man':
+               {'name': 'Tony Stark',
+                'superpower': 'intelligence',
+                'born': 1970,
+                'member of Avengers': True,
+                'significant other': 'Pepper Potts'},
+               'Aquaman':
+               {'name': 'Arthur Curry',
+                'superpower': 'breath underwater',
+                'born': 1986}}
+
+
+class TestDatabaseSharedHeadings(TestCase):
+    def test_database_shared_headings_return_type(self):
+        self.assertIsInstance(database_shared_headings(superheroes), set)
+
+    def test_database_shared_headings_correct_values_in_set(self):
+        self.assertEqual(database_shared_headings(superheroes), {'name', 'superpower', 'born'})
+
