b593c0f449c9d1145c2e5ca31d19a265aade5012
krystal2694 <pwong2632my.bcit.ca>
2019-04-09 21:15:24 -0700

    fixed function to eliminate loss of precision

--- A5/q05.py
+++ A5/q05.py
@@ -12,17 +12,18 @@ def cashmoney(amount: float)-> dict:
     PRECONDITION: amount must be a positive floating point number
     POSTCONDITION: determine the fewest number of each bill and coin to represent amount
     >>> cashmoney(56.23)
-    {100: 0, 50: 1, 20: 0, 10: 0, 5: 1, 2: 0, 1: 1, 0.25: 0, 0.1: 2, 0.05: 0, 0.01: 2}
+    {100: 0, 50: 1, 20: 0, 10: 0, 5: 1, 2: 0, 1: 1, 0.25: 0, 0.1: 2, 0.05: 0, 0.01: 3}
     >>> cashmoney(478.14)
-    {100: 4, 50: 1, 20: 1, 10: 0, 5: 1, 2: 1, 1: 1, 0.25: 0, 0.1: 1, 0.05: 0, 0.01: 3}
+    {100: 4, 50: 1, 20: 1, 10: 0, 5: 1, 2: 1, 1: 1, 0.25: 0, 0.1: 1, 0.05: 0, 0.01: 4}
     """
     denominations = {100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0, 0.25: 0, 0.10: 0, 0.05: 0, 0.01: 0}
     if isinstance(amount, float) is False or amount <= 0:
         raise ValueError('money must be a positive floating point number.')
     else:
+        amount *= 100
         for key in denominations.keys():
-            denominations[key] = int(amount // key)
-            amount %= key
+            denominations[key] = int(amount // (key * 100))
+            amount %= (key * 100)
 
         return denominations
 
